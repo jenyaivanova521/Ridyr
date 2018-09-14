075ec8a296303b64ae3be9b4ea85d03197f2a8c5
Jenya Ivanova <firepmi320@gmail.com>
2018-09-13 19:43:53 -0700

    register

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,12 +1,15 @@
 import { LoginComponent } from "./pages/login/login.component";
 import { SplashComponent } from "./pages/splash/splash.component";
+import { SignupComponent } from "./pages/signup/signup.component";
 
 export const routes = [
   { path: "", component: SplashComponent},
   { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
 ];
 
 export const navigatableComponents = [
   SplashComponent,
   LoginComponent,
+  SignupComponent,
 ];
